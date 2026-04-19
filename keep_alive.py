cf9475de304fe83ce24365aa4510f3db6f74021e
abrormamajonov77 <abrormamajonov77@gmail.com>
2026-04-19 22:02:13 +0500

    Add files via upload

---
+++ keep_alive.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask(name)
+
+@app.route('/')
+def home():
+    return "Bot ishladi va Render taslim bo'ldi!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
